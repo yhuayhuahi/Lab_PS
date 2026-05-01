71abe3ce05427f497affa91fe0f3e4fa10578be0
dev1d123 <dhuamanio@unsa.edu.pe>
2026-04-30 21:55:57 -0500

    feat: add longitude rule to main code

--- LAB02/ejer01/identificador.py
+++ LAB02/ejer01/identificador.py
@@ -1,4 +1,6 @@
 
 def es_identificador_valido(identificador: str) -> bool:
-
+    # Regla 1 – Longitud entre 1 y 6 caracteres
+    if not (1 <= len(identificador) <= 6):
+        return False
     return True
