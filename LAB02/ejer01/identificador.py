fb96322899be75b831dec6e7ae5f10c06133f058
dev1d123 <dhuamanio@unsa.edu.pe>
2026-04-30 21:57:21 -0500

    feat: add rule2 code, the first character must be an letter

--- LAB02/ejer01/identificador.py
+++ LAB02/ejer01/identificador.py
@@ -3,4 +3,10 @@ def es_identificador_valido(identificador: str) -> bool:
     # Regla 1 – Longitud entre 1 y 6 caracteres
     if not (1 <= len(identificador) <= 6):
         return False
+
+    # Regla 2 – El primer carácter debe ser una letra
+    if not identificador[0].isalpha():
+        return False
+
+
     return True
